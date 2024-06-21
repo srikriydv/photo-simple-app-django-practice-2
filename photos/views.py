cbc87d078ec064c76ca2aac64df8421e83dfd1cf
srikriydv <srikri5462ydv@gmail.com>
2024-06-21 11:46:18 +0545

    added all the routes to make django urls work

---
+++ photos/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def gallery(request):
+    context = {}
+    return render(request, 'photos/gallery.html', context)
+
+def viewPhoto(request):
+    context = {}
+    return render(request, 'photos/photos.html', context)
+
+def addPhoto(request):
+    context = {}
+    return render(request, 'photos/add.html', context)
