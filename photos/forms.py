65dce3c9fa1a5089141628ff776dda4f9f00815e
srikriydv <srikri5462ydv@gmail.com>
2024-06-21 20:35:01 +0545

    added link for catagorical selection and others

--- photos/forms.py
+++ photos/forms.py
@@ -1,1 +1,7 @@
 from django.forms import ModelForm
+from .models import Photo
+
+class addPhotoForm(ModelForm):
+    class Meta:
+        model = Photo
+        fields = '__all__'
--- photos/views.py
+++ photos/views.py
@@ -1,14 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Category, Photo
+from .forms import addPhotoForm
 
 # Create your views here.
 def gallery(request):
-    context = {}
+    category_id = request.GET.get('q')
+    if category_id:
+        photos = Photo.objects.filter(category_id=category_id)
+    else:
+        photos = Photo.objects.all()
+
+    categories = Category.objects.all()
+    context = {'categories': categories, 'photos': photos}
     return render(request, 'photos/gallery.html', context)
 
 def viewPhoto(request, pk):
-    context = {}
-    return render(request, 'photos/photos.html', context)
+    photo = Photo.objects.get(id=pk)
+    context = {'photo': photo}
+    return render(request, 'photos/photo.html', context)
 
 def addPhoto(request):
-    context = {}
+    categories = Category.objects.all()
+    if request.method == "POST":
+        data = request.POST
+        image = request.FILES.get('image')
+
+        if data['category'] != 'none':
+            category = Category.objects.get(id=data['category'])
+        elif data['category_new'] != '':
+            category, created = Category.objects.get_or_create(name=data['category_new'])
+        else:
+            category = None
+
+        Photo.objects.create(
+            category=category,
+            description=data['description'],
+            image=image,
+        )
+        return redirect('gallery')
+
+    context = {'categories': categories}
     return render(request, 'photos/add.html', context)
